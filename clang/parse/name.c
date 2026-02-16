16b3f8660de74773b9a2cb9d2c1b61c87e5ec877
Lorenzobattistela <lorenzobattistela@gmail.com>
2026-02-16 09:33:22 -0300

    errors

--- clang/parse/name.c
+++ clang/parse/name.c
@@ -4,14 +4,14 @@ fn u32 parse_name(PState *s) {
   if (!nick_is_init(c)) {
     parse_error(s, "name", c);
   }
-  u32 k = 0;
+  u32 start = s->pos;
   while (nick_is_char(parse_peek(s))) {
-    c = parse_peek(s);
-    k = ((k << 6) + nick_letter_to_b64(c)) & EXT_MASK;
     parse_advance(s);
   }
+  u32 len = s->pos - start;
+  u32 id  = table_find(s->src + start, len);
   parse_skip(s);
-  return k;
+  return id;
 }
 
 // Like parse_name, but returns a unique ID from the global table.
